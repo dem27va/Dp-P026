87a62f89b49cd1e0678c5f24513dea3dd98c0491
dem27va <47898484+dem27va@users.noreply.github.com>
2020-07-20 23:34:16 +0300

    Add files via upload

--- Homework 2020-07-21/HW_2020-07-21_Task1.py
+++ Homework 2020-07-21/HW_2020-07-21_Task1.py
@@ -1,111 +1,112 @@
-numberOfRect = int(input('Введите количество прямоугольников: '))
+numberOfRectangles = int(input('Введите количество прямоугольников: '))
 
-def inputRectLocations(n):                          #Вводим координаты прямоугольников, определяемые двумя угловыми точками
-    locationsArr = []
-    for rectangle in range(n):                      #Перебираем прямоугольники
+def inputRectLocations(numberOrRectangles):                          #Вводим координаты прямоугольников, определяемые двумя угловыми точками
+    rectLocationsList = []
+    for rectangle in range(numberOrRectangles):                      #Перебираем прямоугольники
         twoPointsCoords = {}
         print('Введите координаты двух углов прямоугольника ' + str(rectangle + 1))
         twoPointsCoords['x1'] = int(input('x1: '))
         twoPointsCoords['y1'] = int(input('y1: '))
         twoPointsCoords['x2'] = int(input('x2: '))
         twoPointsCoords['y2'] = int(input('y2: '))
-        locationsArr.append(twoPointsCoords)
+        rectLocationsList.append(twoPointsCoords)
 
-    return locationsArr
-
-rectLocationsArr = inputRectLocations(numberOfRect)
-
-print(rectLocationsArr)
+    return rectLocationsList                             #Возвращаем массив введенных координат прямоугольников
 
 
-def calculateRectCoordinates(inputArr):
+def calculateRectSurfaceCoords(rectLocationsList):             #Формируем множества всех точек прямоугольников по списку координат двух угловых точек каждого прямоугольника
     rectanglesArr = []
     
-    for rectangle in inputArr:
+    for rectangle in rectLocationsList:
         rectCoordinatesSet = set()
         
         for x in range(min(rectangle['x1'], rectangle['x2']), max(rectangle['x1'], rectangle['x2']) + 1):       # +1 потому, что range() не захватывает последне значение и мы теряем координаты
             for y in range(min(rectangle['y1'], rectangle['y2']), max(rectangle['y1'], rectangle['y2']) + 1):
-                pareOfCoordinates = (x, y)                  #записываем в кортеж пару координат каждой точки прямоугольника
-                rectCoordinatesSet.add(pareOfCoordinates)   #добавляем в монжество координаты каждой точки прямоугольника
+                rectCoordinatesSet.add((x, y))   #добавляем в монжество координаты всех точек прямоугольника
         
         rectanglesArr.append(rectCoordinatesSet)
     
-    return rectanglesArr                                    #Возвращаем список множеств, содержащих кортежи, в которых записаны пары координат каждой точки прямоугольника
+    return rectanglesArr                                    #Возвращаем список множеств, содержащих кортежи, в которых записаны пары координат всех точек прямоугольника
 
-res = calculateRectCoordinates(rectLocationsArr)
 
-#print(res)
-#print(res[0])
-for el in res:
-    print(el)
+def calculateRectIntersectArea(rectCoordSetsList):
+    intersections = []
+    intersectionsArea = 0
 
-def drawRect(coordSetArr):
+    for i in range(len(rectCoordSetsList)):             #Получаем координаты точек пересечений, поочередно проверяя пересечения множеств
+        for j in range(i + 1, len(rectCoordSetsList)):
+            if rectCoordSetsList[i] & rectCoordSetsList[j]:
+                intersections.append(rectCoordSetsList[i] & rectCoordSetsList[j])
+
+    for intersection in intersections:                  #Перебираем пересечения в массиве пересечений
+        xs = []                                         #Создаем временный массив со всеми координатами X для текущего пересечения
+        ys= []                                          #Создаем временный массив со всеми координатами Y для текущего пересечения        
+        for coordPare in intersection:                  #Перебираем пары координат каждой точки текущего пересечения
+            xs.append(coordPare[0])                     #Заполняем массив координат X текущего пересечения
+            ys.append(coordPare[1])                     #Заполняем массив координат Y текущего пересечения
+        
+        intersectionsArea += (max(xs) - min(xs)) * (max(ys) - min(ys))
+
+    return intersectionsArea
+
+def calculateGrossrectangle(rectCoordSetsList):
+    grossXs = []
+    grossYs= []
+    
+    for rectangle in rectCoordSetsList: 
+
+        for coordPare in rectangle: 
+            grossXs.append(coordPare[0]) 
+            grossYs.append(coordPare[1])
+
+    grossrectangleCoordinatesSet = set()
+    
+    for x in range(min(grossXs), max(grossXs) + 1):                           # +1 потому, что range() не захватывает последне значение и мы теряем координаты
+        for y in range(min(grossYs), max(grossYs) + 1):
+            grossrectangleCoordinatesSet.add((x, y))     #добавляем в монжество координаты всех точек прямоугольника
+    
+    return grossrectangleCoordinatesSet
+
+
+def drawRect(rectCoordSetsList, grossrectangle):
     import matplotlib.pyplot as plt
     import matplotlib.patches as patches
     fig = plt.figure() 
     ax = fig.add_subplot(111) 
 
-    for rectangle in coordSetArr:           #Перебираем прямоуголники в массиве с координатами
-        xs = []                             #Создаем временный массив со всеми координатами X для текущего прямоугольника
-        ys= []                              #Создаем временный массив со всеми координатами Y для текущего прямоугольника
-        
-        for coordPare in rectangle:         #Перебираем пары координат каждой точки текущего прямоугольника
-            xs.append(coordPare[0])         #Заполняем массив координат X текущего прямоугольника
-            ys.append(coordPare[1])         #Заполняем массив координат Y текущего прямоугольника
+    for rectangle in rectCoordSetsList:                 #Перебираем прямоуголники в массиве с координатами
+        xs = []                                         #Создаем временный массив со всеми координатами X для текущего прямоугольника
+        ys= []                                          #Создаем временный массив со всеми координатами Y для текущего прямоугольника        
+        for coordPare in rectangle:                     #Перебираем пары координат каждой точки текущего прямоугольника
+            xs.append(coordPare[0])                     #Заполняем массив координат X текущего прямоугольника
+            ys.append(coordPare[1])                     #Заполняем массив координат Y текущего прямоугольника
 
-        rect = patches.Rectangle((min(xs), min(ys)), max(xs) - min(xs), max(ys) - min(ys), linewidth=1,edgecolor='r',facecolor='black', alpha = 0.5)
+        rect = patches.Rectangle((min(xs), min(ys)), max(xs) - min(xs), max(ys) - min(ys), linewidth = 1, edgecolor = 'black', facecolor = 'green', alpha = 0.5)
         ax.add_patch(rect)
+    
+    grossXs = []
+    grossYs = []
+    for grossCoord in grossrectangle:
+        grossXs.append(grossCoord[0])
+        grossYs.append(grossCoord[1])
+    
+    rectGross = patches.Rectangle((min(grossXs), min(grossYs)), max(grossXs) - min(grossXs), max(grossYs) - min(grossYs), linewidth = 2, edgecolor = 'red', facecolor = 'none')
+    ax.add_patch(rectGross)
 
     plt.xlim([-50, 50])
     plt.ylim([-50, +50])
     plt.show()
 
-drawRect(res)
 
-'''
-rectCoord1 = set()
-for x in range(min(x1, x2), max(x1,x2) + 1):
-    for y in range(min(y1, y2), max(y1,y2) + 1):
-        pareOfCoordinates = (x, y)
-        rectCoord1.add(pareOfCoordinates)
+rectLocationsList = inputRectLocations(numberOfRectangles)
 
+rectSurfaceCoords = calculateRectSurfaceCoords(rectLocationsList)
 
+print(rectSurfaceCoords)
 
+intersectionsArea = calculateRectIntersectArea(rectSurfaceCoords)
+print('Общая площадь пересечения всех прямоугольников: ', intersectionsArea)
+grossrectangle = calculateGrossrectangle(rectSurfaceCoords)
+print('Координаты глобального прямоугольника: ', grossrectangle)
 
-
-def drawGraph(coordSet):
-    import matplotlib.pyplot as plt
-    xs = []
-    ys = []
-    for pare in coordSet:
-        xs.append(pare[0])
-        ys.append(pare[1])
-    plt.scatter(xs, ys, s = 10)
-    plt.show()
-
-def drawRect(coordSet):
-    import matplotlib.pyplot as plt
-    import matplotlib.patches as patches
-    xs = []
-    ys = []
-    for pare in coordSet:
-        xs.append(pare[0])
-        ys.append(pare[1])
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    rect1 = patches.Rectangle((min(xs), min(ys)), max(xs) - min(xs), max(ys) - min(ys), linewidth=1,edgecolor='r',facecolor='black', alpha = 0.5)
-    ax.add_patch(rect1)
-
-    plt.xlim([min(xs) - 1, max(xs) + 1])
-    plt.ylim([min(ys) - 1, max(ys) + 1])
-
-    plt.show()
-
-
-#drawGraph(rectCoord1)
-drawRect(rectCoord1)
-'''
-
-
+drawRect(rectSurfaceCoords, grossrectangle)
